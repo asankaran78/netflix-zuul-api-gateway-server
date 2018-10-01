9f458f6c06a53a2ea4bbea491717f2d7abf20c8f
Arunachalam <Vijay.Arunachalam@thomsonreuters.com>
2018-09-30 19:15:20 -0500

    Update gateway solution with jwt token update

--- src/main/java/com/usbank/cash/netflixzuulapigatewayserver/ZuulLoggingFilter.java
+++ src/main/java/com/usbank/cash/netflixzuulapigatewayserver/ZuulLoggingFilter.java
@@ -1,7 +1,8 @@
 package com.usbank.cash.netflixzuulapigatewayserver;
 
+import java.util.Date;
+
 import javax.servlet.http.HttpServletRequest;
-import javax.xml.ws.spi.http.HttpContext;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,10 +12,18 @@ import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+
 @Component
 public class ZuulLoggingFilter extends ZuulFilter {
 	
-	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    
+    private long EXPIRATIONTIME = 1000 * 60 * 5; 
+    private String secret = "ThisIsASecret";
 
 	@Override
 	public boolean shouldFilter() {
@@ -24,11 +33,9 @@ public class ZuulLoggingFilter extends ZuulFilter {
 	@Override
 	public Object run() throws ZuulException {
 		
-		HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
-		
-		logger.info("request -> {} requestURI->",request,request.getRequestURI());
-		
+		createAndAttachJwtToken(RequestContext.getCurrentContext());	
 		return null;
+	
 	}
 
 	@Override
@@ -40,5 +47,19 @@ public class ZuulLoggingFilter extends ZuulFilter {
 	public int filterOrder() {
 		return 1;
 	}
+	
+	private void createAndAttachJwtToken(RequestContext context) {
+		
+        HttpServletRequest request = context.getRequest();
+        String userName = request.getHeader("username");
+
+        String authorizationHeader = Jwts.builder()
+                 .setSubject(userName)
+                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATIONTIME))
+                 .signWith(SignatureAlgorithm.HS512, secret)
+                 .compact();
+    
+        context.addZuulRequestHeader(AUTHORIZATION_HEADER, authorizationHeader);
+    }
 
 }
